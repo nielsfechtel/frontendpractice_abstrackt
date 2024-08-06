63436f8fc737cce69823b1b71a48c075d3f1e52b
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2024-08-06 21:42:55 +0200

    (feat) add css-variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-bg1': 'hsl(var(--color-bg1) / <alpha-value>)',
+        'color-bg2': 'hsl(var(--color-bg2) / <alpha-value>)',
+        'color-bg3': 'hsl(var(--color-bg3) / <alpha-value>)',
+        'color-primary1': 'hsl(var(--color-primary1) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 }
-
